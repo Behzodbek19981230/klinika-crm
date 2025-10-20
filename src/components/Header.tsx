24eab86ceb06da4c086dbc09fe71b61a429c7f49
Zarnigor0708 <zarnigorochilova192@gmail.com>
2025-10-20 20:47:37 +0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header className="fixed top-0 w-full bg-white/95 backdrop-blur-sm  z-50 mx-16">
+            <div className="container px-4 py-4 flex items-center justify-between">
+                <div className="flex items-center space-x-4">
+                    <div className="">
+                        <Link to="/">Klinika <b>CRM</b></Link>
+                    </div>
+                    {/* Desktop Navigation */}
+                    <nav className="flex md:flex items-center">
+
+                        <a href="#pricing" className="text-muted-foreground hover:text-foreground transition-colors">
+                            Shifokorlar
+                        </a>
+                        <a href="#about" className="text-muted-foreground hover:text-foreground transition-colors">
+                            Foydalanuvchilar
+                        </a>
+                        <a href="#contact" className="text-muted-foreground hover:text-foreground transition-colors">
+                            Bemorlar
+                        </a>
+                    </nav>
+                </div>
+
+                <div className=" md:flex items-center space-x-2">
+                    <Button variant="outline" className="border-none bg-[#a6c1ee] hover:bg-[#87acec">
+                        <Link to="/login">Logout</Link>
+                    </Button>
+                </div>
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
